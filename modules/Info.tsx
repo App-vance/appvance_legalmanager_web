3ee8d1e7911c460f1ce7e3d587fca21fe0f91cfe
John James Gallego Hernandez <jamesgallegoh@Mac-mini-de-John.local>
2025-08-18 19:03:56 -0500

    style: styles for info module

--- modules/Info.tsx
+++ modules/Info.tsx
@@ -4,8 +4,8 @@ import { ExclamationCircleIcon, DocumentTextIcon, CurrencyDollarIcon, CreditCard
 export default function Info() {
   return (
     <>
-      <section className="bg-[#F1FAFF] flex px-9 py-16 lg:h-[700px] lg:p-20 lg:gap-6">
-        <div className="w-full lg:flex-1 lg:h-full">
+      <section className="bg-[#F1FAFF] flex flex-col justify-between lg:flex-row lg:h-[700px] lg:p-24 lg:gap-6">
+        <div className="w-full px-9 py-16 lg:flex-1 lg:h-full lg:px-0 lg:py-0 lg:max-h-[500px]">
           <div className="flex flex-col gap-6 lg:h-full">
             <div className="flex-1 border-b border-gray lg:border-none last:border-b-0 xl:max-w-[560px]">
               <div className="flex flex-col lg:flex-row lg:items-center lg:gap-10">
@@ -56,12 +56,12 @@ export default function Info() {
             </div>
           </div>
         </div>
-        <div className="hidden lg:block lg:flex-1 lg:relative">
+        <div className="relative px-0 w-full h-[350px] lg:flex-1 lg:h-[500px]">
           <Image
             src="/img/image-legal-manager-info.webp"
             alt="Ejemplo"
             fill
-            className="rounded-lg max-w-[632px] max-h-[500px] xl:mx-auto"
+            className="!rounded-none lg:rounded-lg lg:max-w-[632px] lg:max-h-[500px] lg:ml-auto"
           />
         </div>
       </section>
