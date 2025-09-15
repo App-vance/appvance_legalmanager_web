f0b4c55b07733cb45fc0bd57b94c6dce162fad6f
John Elias Trujillo Figueroa <john.trujillo@wompi.com>
2025-09-15 14:45:50 -0500

    style: refactor team carousel to use new Carousel component and update phases helper

---
+++ helpers/phases.tsx
@@ -0,0 +1,14 @@
+export const phases = [
+  {
+    author: "Nelson Mandela",
+    text: "La educación es el arma más poderosa que puedes usar para cambiar el mundo.",
+  },
+  {
+    author: "Mandela",
+    text: "La educación es el arma más poderosa que puedes usar para cambiar el mundo.",
+  },
+  {
+    author: "Nelson",
+    text: "La educación es el arma más poderosa que puedes usar para cambiar el mundo.",
+  },
+];
